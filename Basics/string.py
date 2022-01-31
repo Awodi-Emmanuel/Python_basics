0b2f87a1e6e1ee0ccc7aff0dac3b767739f0f9f2
emmy70 <muhddev@gmail.com>
2022-01-31 22:49:21 +0100

    String and slice completed

--- Basics/string.py
+++ Basics/string.py
@@ -1,2 +1,30 @@
-mySting = 'abcdef'
-print(mySting[-3:])
+ # STRINGS
+mySting = 'abcdefg'
+print(mySting[-4])
+    
+ 
+#  INDEXING 
+
+print(mySting[-4])
+
+# SLICING
+
+print(mySting[4:])
+
+# REVERSING A SLICING STRING 
+
+print(mySting[:4])
+
+my_slicing = 'Hello world'
+
+print(my_slicing[::2]) 
+
+# BASIC METHODS 
+
+x = my_slicing.split('l')
+
+print(x)
+# PRINTING FORMATTING 
+ X = "Item One: {x} Item Two: {y}".format(x ="dog",y="cat")
+
+print(x) 
