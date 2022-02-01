578cd4210abbd54799f43b773c5ac935fab5f6c3
emmy70 <muhddev@gmail.com>
2022-02-01 13:05:23 +0100

    completed list comprehension

--- Basics/list.py
+++ Basics/list.py
@@ -16,4 +16,17 @@ print("After reasignments")
 
 print((list_assign))
 
+# NEST LIST
+
+mylist = [1, 2, 3,['a', 'b', 'c']]
+
+print(mylist[1])
+
+# LIST COMPREHENSION  
+
+matrix = [[1,2,3,], [4,5,6,],[7,8,9]]
+
+first_col = [row[2] for row in matrix]
+
+print(first_col)
 
