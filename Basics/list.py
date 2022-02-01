f33dd0eee3cd8d7a00e77b4a66d1e74be2b27aad
emmy70 <muhddev@gmail.com>
2022-02-01 12:24:19 +0100

    exampe of mutable list

--- Basics/list.py
+++ Basics/list.py
@@ -1,3 +1,19 @@
-my_list = [1, 2, 3, 4]
+from audioop import reverse
 
+
+my_list = [1, 2, 3, 4]
 print(my_list)
+mylist = ['Django', 'Nodejs', 'Laravel']
+print(len(mylist))
+
+# LIST REASSIGNMENT
+
+list_assign = ['a', 'b', 'c', 'd', 'e']
+print('before reasignment:')
+
+list_assign[0] = 'New Item:'
+print("After reasignments")
+
+print((list_assign))
+
+
